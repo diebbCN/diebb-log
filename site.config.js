68019d7ef457fa15d0091bdd078d4d8c44781a5b
蝶白白 <97093581+diebbCN@users.noreply.github.com>
2024-07-28 21:04:27 +0800

    Update site.config.js
    add the repo for storing comments

--- site.config.js
+++ site.config.js
@@ -56,7 +56,8 @@ const CONFIG = {
   utterances: {
     enable: true,
     config: {
-      repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
+      // repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
+      repo: "diebbCN/diebb-log-comments",
       "issue-term": "og:title",
       label: "💬 Utterances",
     },
